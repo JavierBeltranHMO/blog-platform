1164809d270048a75af2e583801934fe4d62c3ea
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-11 17:25:04 -0700

    added devise

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
-
+before_action :authenticate_user!, only: %i[new update destroy]
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
