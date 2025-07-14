7188d06167b297a3f3adff9c5e390746f09434c4
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-14 14:33:08 -0700

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get "users/show"
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
   resources :blogs
   get 'myblogs', as: 'blogs/myblogs', to: 'blogs#myblogs'
+
+  resources :users, only:[:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
