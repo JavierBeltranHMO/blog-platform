473e74778c9b458e405ea4cda2247d1a68d8081c
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-18 16:47:51 -0700

    admin panel works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get "dashboard/index"
+  end
   get "users/show"
   devise_for :users, controllers: {
         registrations: "users/registrations"
@@ -6,19 +9,14 @@ Rails.application.routes.draw do
   resources :blogs
   get "myblogs", as: "blogs/myblogs", to: "blogs#myblogs"
 
+  namespace :admin do
+    get "dashboard", to: "dashboard#index"
+  end
+
   delete "cancel_account", to: "users#cancel_account", as: :cancel_account
 
   resources :users, only: [ :show ]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-    # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-    # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-    # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-    # Defines the root path route ("/")
-    root "blogs#index"
+  root "blogs#index"
 end
