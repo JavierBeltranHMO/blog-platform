c5f3215f6ec257b1490c74bb1114b3a371b003d1
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-18 12:40:02 -0700

    added creation date to blogs

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,11 @@
 module BlogsHelper
+  include ActionView::Helpers::DateHelper
+
+  def formatted_blog_date(blog)
+    if blog.created_at>=6.days.ago
+      "#{time_ago_in_words(blog.created_at)} ago"
+    else
+      blog.created_at.strftime("%B %d, %Y")
+    end
+  end
 end
