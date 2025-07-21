c910ce24e629990e30ab6e55b89638e0b0552fe1
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-21 15:03:32 -0700

    users can add a bio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_18_212121) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_215048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_18_212121) do
     t.string "user_name"
     t.string "slug"
     t.string "role", default: "user"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
